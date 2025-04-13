27e02394f1081d7622cba979109b505006deb849
starday119 <jy8294@naver.com>
2025-04-13 23:12:56 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/snapmeal/snapmeal/SnapmealApplication.java
@@ -0,0 +1,13 @@
+package snapmeal.snapmeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapmealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapmealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/snapmeal/snapmeal/SnapmealApplicationTests.java
@@ -0,0 +1,13 @@
+package snapmeal.snapmeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapmealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
